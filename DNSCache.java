bdb2d24ebca78685f5f9b60a8fe869c0c27c5848
Brett Greenman <greenmbr@mail.gvsu.edu>
2015-10-26 10:54:32 -0400

    Skeleton of DNSCache added

--- DNSCache.java
+++ DNSCache.java
@@ -2,6 +2,7 @@ import java.io.*;
 import java.net.*;
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.HashMap<K,V>
 
 /********************************************************************************
 * CIS 457
@@ -14,5 +15,28 @@ import java.util.ArrayList;
 ********************************************************************************/
 
 class DNSCache {
+    HashMap<String,String> completeDomainCache;
+    //  HashMap<String,String[]> subDomainCache = new HashMap<String,String[]>;
 
+    public DNSCache(){
+	completeDomainCache = new HashMap<String,String>;
+    }
+    public String getIP(String domain){
+	return completeDomain.get(domain);
+    }
+
+    public appendComplete(String domain, String ip){
+	
+    }	
+
+
+
+    public boolean checkCompleteDomain(String s){
+	//NEED TO ADD CHECK FOR TTL
+	return completeDomainCache.containsKey(s); //tells whether or not complete name is stored in cache
+    }
+
+    public boolean checkSubDomain(String s){
+
+    }
 }
